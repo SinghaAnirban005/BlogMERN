c58f0f960a8cbd8965ca14cd314d106b3e615cc5
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2024-07-15 09:38:02 +0000

    Fixed footer date

--- frontend/blog/src/components/Footer/Footer.jsx
+++ frontend/blog/src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Footer() {
-  const presentYear = Date.now();
+  const presentYear = new Date();
 
   return (
     <footer className="flex justify-center bg-slate-400 h-[10em] items-center">
       <h2>
-        Copyright (C) 2023- {`${presentYear.getFullYear}`} by Anirban Singha
+        Copyright (C) 2023 - {`${presentYear.getFullYear()}`} by Anirban Singha
       </h2>
     </footer>
   );
